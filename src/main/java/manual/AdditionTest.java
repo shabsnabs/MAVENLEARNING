9bd1541ab794f0cf1da7423a3484e522a755f5fd
shabsnabs <shabanassl@gmail.com>
2023-03-23 11:53:27 +0530

    changed

--- src/main/java/manual/AdditionTest.java
+++ src/main/java/manual/AdditionTest.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class AdditionTest {
 	public static void main(String arg[] ){
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter first value");		
-	int a = sc.nextInt();
-	System.out.println("Enter Second value");		
-	int b = sc.nextInt();
-	int x =a+b;
-	System.out.println("Addition Result is " +x);
-	
-	}
+	System.out.println("Addition Result is ");}
+//		Scanner sc= new Scanner(System.in);
+//		System.out.println("Enter first value");		
+//	int a = sc.nextInt();
+//	System.out.println("Enter Second value");		
+//	int b = sc.nextInt();
+//	int x =a+b;
+//	System.out.println("Addition Result is " +x);
+//	
+//	}
 }
